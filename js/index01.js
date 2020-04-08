1b2f5f30ff050d6f06b0ed52cecba4897ca3dd17
shannon54321 <248128459@qq.com>
2020-04-08 12:11:51 +0800

    turnPage

--- js/index01.js
+++ js/index01.js
@@ -1,7 +1,7 @@
 var curPage = 1;
-var pageSize = 15;
+var pageSize = 4;
 var tableData = [];
-
+var allPage = 1;
 
 function bindEvent(){
 	$('#menu-list').on('click','dd',function(){
@@ -11,6 +11,7 @@ function bindEvent(){
 		var id = $(this).data('id');
 
 		if (id == 'student-list') {
+            curPage = 1;
             getTableData();
         }
 
@@ -26,9 +27,12 @@ function bindEvent(){
 		e.preventDefault();
 		var data = $('#add-student-form').serializeArray();
 		data = checkData(data);
-		if (data) {
+		if (!data) {
+            alert('请将信息填写完全后提交');
+        } else {
 			transferData('/api/student/addStudent', data, function (res) {
                 alert('提交成功');
+                $('#add-student-form')[0].reset();
                 $('#menu-list > dd[data-id=student-list]').trigger('click');
             })
 		}
@@ -49,7 +53,7 @@ function bindEvent(){
     });
     $('#mask').click(function (e) {
         $('#modal').slideUp();
-    })
+    });
     $('#tbody').on('click', '.btn', function () {
         var isEdit = $(this).hasClass('edit');
         var index = $(this).data('index');
@@ -69,9 +73,18 @@ function bindEvent(){
             }
         }
 
+    });
+    $('#search-btn').click(function () {
+        var val = $('#search-inp').val();
+
+        // 将当前页置为1 获取表格数据
+        curPage = 1;
+        getTableData(val);
     })
 
 }
+
+
 function renderEditModel(data) {
     var form = $('#edit-student-form')[0];
     for (var prop in data) {
@@ -82,16 +95,64 @@ function renderEditModel(data) {
 }
 
 //  获取表格数据
-function getTableData() {
-    transferData('/api/student/findByPage', {
-        page: curPage,
-        size: pageSize
-    }, function (res) {
-        console.log(res);
-        tableData = res.findByPage;
-        renderTable(res.findByPage);
-    })
+
+// 获取学生列表数据
+function getTableData(val) {
+    // 过滤表格数据
+    if (val) {
+        // 默认不分性别
+        var sex = -1;
+        // 搜索关键字为输入框内数据
+        var search = val.trim();
+        // 如果输入框内有男/ 女关键字  则修改性别属性 以及搜索关键字
+        if (val.indexOf('男') != -1) {
+            sex = 0;
+            search = val.replace('男', '').trim();
+        }
+        if (val.indexOf('女') != -1) {
+            sex = 1;
+            search = val.replace('女', '').trim();
+        }
+        transferData('/api/student/searchStudent', {
+            page: curPage,
+            size: pageSize,
+            search: search ? search : null,
+            sex: sex
+        }, function (res) {
+            // 保存表格数据
+            
+            tableData = res.searchList;
+            allPage = Math.ceil(res.cont / pageSize);
+            renderTable(res.searchList);
+        })
+    // 不过滤表格数据
+    } else {
+        transferData('/api/student/findByPage', {
+            page: curPage,
+            size: pageSize
+        }, function (res) {
+            // 保存表格数据
+            tableData = res.findByPage;
+            allPage = Math.ceil(res.cont / pageSize);
+            renderTable(res.findByPage);
+        });
+    }
+    
 }
+
+// function getTableData() {
+//     transferData('/api/student/findByPage', {
+//         page: curPage,
+//         size: pageSize,
+//         search: search ? search : null,
+//         sex: sex
+//     }, function (res) {
+//         console.log(res);
+//         tableData = res.findByPage;
+//         allPage = Math.ceil(res.cont / pageSize);
+//         renderTable(res.findByPage);
+//     })
+// }
 // 渲染表格数据
 function renderTable(data) {
     var str = '';
@@ -111,6 +172,15 @@ function renderTable(data) {
     </tr>'
     });
     $('#tbody').html(str);
+    $('#page').turnpage({
+        curPage: curPage,
+        allPage: allPage,
+        callback: function (page) {
+            curPage = page;
+            getTableData($('#search-inp').val())
+            
+        }
+    })
 }
 
 
@@ -134,21 +204,21 @@ function transferData(url,data,cb){
 	})
 
 }
-
+// 将表单数据转换成对象
 function checkData(data){
 	var obj = {};
-	var flag = true;
-	data.forEach(function(item,index){
-		obj[item.name] = item.value;
-		if (!item.value) {
-			flag = false;
-		}
-	});
-	if (!flag) {
-		return false;
-
-	}
-	return obj;
+    // for循环的数据可以是数组  也可以是对象（类数组）
+    // forEach循环只能是数组
+    for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+        if (!item.value) {
+            // 如果是数组的forEach  或者map  filter  方法中return的话  代表的是方法的返回值  
+            // 否则是最近一层函数的返回值
+            return false;
+        }
+        obj[item.name] = item.value;
+    }
+    return obj;
 }
 
 
